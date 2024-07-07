6e82513d8a944371c643d1ddecb8b3b14c4014ee
Jorge Tapia <283672+NSKomodo@users.noreply.github.com>
2024-07-07 15:50:53 -0400

    Add workout model class

---
+++ ORM/ORM/Model/Workout.swift
@@ -0,0 +1,21 @@
+//
+//  Workout.swift
+//  ORM
+//
+//  Created by Jorge Tapia on 7/7/24.
+//
+
+import Foundation
+
+/// Represents a workout row inside the historical data file.
+struct Workout: Codable {
+    /// The date the workout was completed.
+    let date: Date
+    /// The name of exercise.
+    let exercise: String
+    /// The number of repetitions.
+    let repetitions: Int
+    /// The weight used during the exercise.
+    let weight: Double
+}
+
