22c84fae803d87148f8f7faea3148abeaf0833ef
Jorge Tapia <283672+NSKomodo@users.noreply.github.com>
2024-07-07 15:33:04 -0400

    Add base project files

---
+++ ORM/ORM/ORMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ORMApp.swift
+//  ORM
+//
+//  Created by Jorge Tapia on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ORMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
