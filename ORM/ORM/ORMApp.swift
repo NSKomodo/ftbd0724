3e3a1fde317aa877ffb7281c5c8e29e530b96503
Jorge Tapia <283672+NSKomodo@users.noreply.github.com>
2024-07-07 23:44:49 -0400

    Create view stub for One Mar Rep list screen

--- ORM/ORM/ORMApp.swift
+++ ORM/ORM/ORMApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ORMApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            OneRepMaxListView()
         }
     }
 }
---
+++ ORM/ORM/View/One Rep Max List/OneRepMaxListView.swift
@@ -0,0 +1,25 @@
+//
+//  OneRepMaxListView.swift
+//  ORM
+//
+//  Created by Jorge Tapia on 7/7/24.
+//
+
+import SwiftUI
+
+struct OneRepMaxListView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    OneRepMaxListView()
+}
+
